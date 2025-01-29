e4a7c5ee9b9b37a079df328dabfd1bcdeb192ab5
Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>
2025-01-29 18:06:30 +0100

    netfilter: conntrack: Bound nf_conntrack sysctl writes
    BugLink: https://bugs.launchpad.net/bugs/2133301
    
    [ Upstream commit 8b6861390ffee6b8ed78b9395e3776c16fec6579 ]
    
    nf_conntrack_max and nf_conntrack_expect_max sysctls were authorized to
    be written any negative value, which would then be stored in the
    unsigned int variables nf_conntrack_max and nf_ct_expect_max variables.
    
    While the do_proc_dointvec_conv function is supposed to limit writing
    handled by proc_dointvec proc_handler to INT_MAX. Such a negative value
    being written in an unsigned int leads to a very high value, exceeding
    this limit.
    
    Moreover, the nf_conntrack_expect_max sysctl documentation specifies the
    minimum value is 1.
    
    The proc_handlers have thus been updated to proc_dointvec_minmax in
    order to specify the following write bounds :
    
    * Bound nf_conntrack_max sysctl writings between SYSCTL_ZERO
      and SYSCTL_INT_MAX.
    
    * Bound nf_conntrack_expect_max sysctl writings between SYSCTL_ONE
      and SYSCTL_INT_MAX as defined in the sysctl documentation.
    
    With this patch applied, sysctl writes outside the defined in the bound
    will thus lead to a write error :
    
    ```
    sysctl -w net.netfilter.nf_conntrack_expect_max=-1
    sysctl: setting key "net.netfilter.nf_conntrack_expect_max": Invalid argument
    ```
    
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- net/netfilter/nf_conntrack_standalone.c
+++ net/netfilter/nf_conntrack_standalone.c
@@ -620,7 +620,9 @@ static struct ctl_table nf_ct_sysctl_table[] = {
 		.data		= &nf_conntrack_max,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_INT_MAX,
 	},
 	[NF_SYSCTL_CT_COUNT] = {
 		.procname	= "nf_conntrack_count",
@@ -656,7 +658,9 @@ static struct ctl_table nf_ct_sysctl_table[] = {
 		.data		= &nf_ct_expect_max,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ONE,
+		.extra2		= SYSCTL_INT_MAX,
 	},
 	[NF_SYSCTL_CT_ACCT] = {
 		.procname	= "nf_conntrack_acct",
@@ -950,7 +954,9 @@ static struct ctl_table nf_ct_netfilter_table[] = {
 		.data		= &nf_conntrack_max,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_INT_MAX,
 	},
 	{ }
 };
