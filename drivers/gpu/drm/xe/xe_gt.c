378fa19f62acfbda6c02416ca74e5f3e14d9dcae
Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
2025-05-20 19:54:45 +0530

    drm/xe/mocs: Initialize MOCS index early
    BugLink: https://bugs.launchpad.net/bugs/2136221
    
    commit 2a58b21adee3df10ca6f4491af965c4890d2d8e3 upstream.
    
    MOCS uc_index is used even before it is initialized in the following
    callstack
        guc_prepare_xfer()
        __xe_guc_upload()
        xe_guc_min_load_for_hwconfig()
        xe_uc_init_hwconfig()
        xe_gt_init_hwconfig()
    
    Do MOCS index initialization earlier in the device probe.
    
    Signed-off-by: Balasubramani Vivekanandan <balasubramani.vivekanandan@intel.com>
    Reviewed-by: Ravi Kumar Vodapalli <ravi.kumar.vodapalli@intel.com>
    Link: https://lore.kernel.org/r/20250520142445.2792824-1-balasubramani.vivekanandan@intel.com
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    (cherry picked from commit 241cc827c0987d7173714fc5a95a7c8fc9bf15c0)
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Stable-dep-of: 3155ac89251d ("drm/xe: Move page fault init after topology init")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>

--- drivers/gpu/drm/xe/xe_gt.c
+++ drivers/gpu/drm/xe/xe_gt.c
@@ -318,6 +318,8 @@ int xe_gt_init_early(struct xe_gt *gt)
 	xe_wa_process_oob(gt);
 	xe_tuning_process_gt(gt);
 
+	xe_mocs_init_early(gt);
+
 	return 0;
 }
 
@@ -508,8 +510,6 @@ int xe_gt_init(struct xe_gt *gt)
 	err = xe_gt_pagefault_init(gt);
 	if (err)
 		return err;
-
-	xe_mocs_init_early(gt);
 
 	xe_gt_sysfs_init(gt);
 
